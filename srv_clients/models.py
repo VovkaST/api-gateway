a4899d7d54bd9c27dfab97a3c693c86d3e0a9644
Suntsov Vladimir <vovka.steel911@mail.ru>
2023-01-09 20:37:57 +0300

    BaseModel added to client models

--- srv_clients/models.py
+++ srv_clients/models.py
@@ -1,13 +1,30 @@
 from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm.collections import InstrumentedList
 
 from srv_clients.database import Base
 
 
-class Client(Base):
+class BaseModel(Base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, comment="Идентификатор")
+
+    def to_dict(self) -> dict:
+        data = dict()
+        for k, v in self.__dict__.items():
+            if k.startswith("_"):
+                continue
+            elif isinstance(v, Base):
+                v = v.to_dict()
+            elif isinstance(v, InstrumentedList):
+                v = [item.to_dict() for item in v]
+            data[k] = v
+        return data
+
+
+class Client(BaseModel):
     __tablename__ = "client"
     __table_args = {"comment": "Таблица клиентов"}
 
-    id = Column(Integer, primary_key=True, comment="Идентификатор")
     surname = Column(Text(length=255), nullable=False, comment="Фамилия")
     name = Column(Text(length=255), nullable=False, comment="Имя")
     country = Column(Text(length=255), comment="Страна")
