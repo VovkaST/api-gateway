6a622cb945ceadfe8abe419923a4a9689c4d5301
Suntsov Vladimir <vovka.steel911@mail.ru>
2023-01-11 21:07:46 +0300

    Filter endpoint added to clients

--- srv_clients/main.py
+++ srv_clients/main.py
@@ -1,13 +1,11 @@
 from typing import List
 
-from fastapi import FastAPI, HTTPException
-from sqlalchemy import select
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.params import Query
 from starlette import status
-from starlette.requests import Request
 
 from srv_clients.database import Base, engine
 from srv_clients.database import sm as session_maker
-from srv_clients.models import Client
 from srv_clients.querysets import ClientQueryset
 from srv_clients.schemas import ClientSchema, ClientViewSchema
 
@@ -29,37 +27,55 @@ async def on_shutdown():
     await app.state.engine.dispose()
 
 
-@app.get("/list", response_model=List[ClientViewSchema])
-async def get_all_clients(request: Request):
+@app.get("/clients", response_model=List[ClientViewSchema])
+async def get_filtered_clients(
+    request: Request,
+    surname: str = Query(None, description="Фамилия"),
+    name: str = Query(None, description="Имя"),
+    country: str = Query(None, description="Страна"),
+    limit: int = Query(
+        None, description="Количество записей на странице результатов"
+    ),
+    offset: int = Query(
+        None,
+        description="Смещение страницы результатов относительно первой записи",
+    ),
+):
     async with request.app.state.session_maker() as session:
-        rows = await session.scalars(select(Client))
-        return [row.__dict__ for row in rows.all()]
-
-
-@app.get("/{client_id}", response_model=ClientViewSchema)
-async def get_client(request: Request, client_id: int):
-    async with request.app.state.session_maker() as session:
-        client = await ClientQueryset.get_by_id(session, client_id)
-        return client.__dict__
+        rows = await ClientQueryset.get_multiple(
+            session,
+            surname=surname,
+            name=name,
+            country=country,
+            limit=limit,
+            offset=offset,
+        )
+        return [row.to_dict() for row in rows.all()]
 
 
 @app.post(
-    "", response_model=ClientViewSchema, status_code=status.HTTP_201_CREATED
+    "/clients",
+    response_model=ClientViewSchema,
+    status_code=status.HTTP_201_CREATED,
 )
 async def create_client(request: Request, data: ClientSchema):
     sm = request.app.state.session_maker
     async with sm.begin() as session:
-        created = Client(**data.dict())
-        session.add(created)
-        await session.flush([created])
-        id_ = created.id
+        id_ = await ClientQueryset.create(session, **data.dict())
     async with sm() as session:
         client = await ClientQueryset.get_by_id(session, id_)
-        return client.__dict__
+        return client.to_dict()
+
+
+@app.get("/clients/{client_id}", response_model=ClientViewSchema)
+async def get_client(request: Request, client_id: int):
+    async with request.app.state.session_maker() as session:
+        client = await ClientQueryset.get_by_id(session, client_id)
+        return client.to_dict()
 
 
 @app.patch(
-    "/{client_id}",
+    "/clients/{client_id}",
     response_model=ClientViewSchema,
     status_code=status.HTTP_202_ACCEPTED,
 )
@@ -75,11 +91,11 @@ async def update_client(request: Request, client_id: int, data: ClientSchema):
 
     async with sm() as session:
         client = await ClientQueryset.get_by_id(session, client_id)
-        return client.__dict__
+        return client.to_dict()
 
 
 @app.delete(
-    "/{client_id}",
+    "/clients/{client_id}",
     status_code=status.HTTP_202_ACCEPTED,
 )
 async def delete_client(request: Request, client_id: int):
--- srv_clients/querysets.py
+++ srv_clients/querysets.py
@@ -8,9 +8,35 @@ class ClientQueryset:
     model = Client
 
     @classmethod
+    async def create(cls, session: AsyncSession, **kwargs) -> int:
+        created = cls.model(**kwargs)
+        session.add(created)
+        await session.flush([created])
+        return created.id
+
+    @classmethod
     async def get_by_id(cls, session: AsyncSession, id_: int) -> Client:
         return await session.scalar(
             select(cls.model).where(cls.model.id == id_)
+        )
+
+    @classmethod
+    async def get_multiple(
+        cls,
+        session: AsyncSession,
+        limit: int = None,
+        offset: int = None,
+        **filters,
+    ):
+        where = list()
+        if surname := filters.get("surname"):
+            where.append(cls.model.surname == surname)
+        if name := filters.get("name"):
+            where.append(cls.model.name == name)
+        if country := filters.get("country"):
+            where.append(cls.model.country == country)
+        return await session.scalars(
+            select(cls.model).where(*where).limit(limit).offset(offset)
         )
 
     @classmethod
